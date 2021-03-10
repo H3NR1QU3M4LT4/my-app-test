ff3e6e17b981dfcb00c47c7b9eb4eb0b22455c5e
1HenriqueMata1 <57409783+1HenriqueMata1@users.noreply.github.com>
2021-03-10 15:16:42 +0000

    first commit

---
+++ src/components/Front.vue
@@ -0,0 +1,34 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button text="Add task" color="green"/>
+        <Button text="Add task" color="blue"/>
+        <Button text="Add task" color="red"/>
+    </header>
+</template>
+
+<script>
+
+import Button from './Button.vue'
+
+export default {
+    name: 'Front',
+    props: ['title'],
+    //or props
+    //props:{
+    //  title: String,
+    //},
+    components: {
+        Button
+    }
+}
+</script>
+
+<style scoped>
+    Front {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
